d9bb5a8ad3935e062fa059eb410e77a6a04ee915
Selvin Naz <Selvin.Naz@fox.com>
2017-09-11 10:39:41 -0600

    Now yes

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'test-iosv2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for test-iosv2
+  pod 'Alamofire', '~> 4.4.0'
+
+end
---
+++ test-iosv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-iosv2
+//
+//  Created by Selvin Naz on 9/11/17.
+//  Copyright © 2017 Selvin Naz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
