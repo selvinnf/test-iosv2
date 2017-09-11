fa01c997330780b9ffde5be403b8e3d3346a5055
Erick J. Pac <erick.pac@fox.com>
2017-09-11 10:45:24 -0600

    Add new file ExampleViewController

---
+++ test-iosv2/ExampleViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ExampleViewController.swift
+//  test-iosv2
+//
+//  Created by Erick Pac on 11/09/17.
+//  Copyright © 2017 Selvin Naz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExampleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Load view")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
